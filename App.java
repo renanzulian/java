a0651d039aacc4b6f0e9d29c0bdf0b74eb0bde75
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-06 17:49:14 -0300

    Fibonacci recursive with static methods

--- App.java
+++ App.java
@@ -1,19 +1,15 @@
 public class App{
-    int limitFibonnaci = 10000;
+    static int limitFibonnaci = 10000;
     public static void main(String[] args) {
-        int previous = 0;
-        int current = 1;
-        int next = previous + current;
-        int limit = Integer.parseInt(args[0]);
+        App.fibonacci(0, 1);
+    }
 
-        System.out.println("--FIBONNACI CHALLENGE--");
-        do {
-            System.out.println(previous);
-            previous = current;
-            current = next;
-            next = previous + current;
-        } while (current < limit);
-        System.out.println("--FINISHED--");
-
+    public static void fibonacci(int previousValue, int currentValue) {
+        System.out.println(currentValue);
+        int nextValue = previousValue + currentValue;
+        previousValue = currentValue;
+        if (nextValue < App.limitFibonnaci) {
+            App.fibonacci(currentValue, nextValue);
+        }
     }
 }
