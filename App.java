3b16aa57228c3b4b8a72e802729d573fad40351b
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-06 15:18:41 -0300

    Fibonnaci

--- App.java
+++ App.java
@@ -1,5 +1,19 @@
 public class App{
+    int limitFibonnaci = 10000;
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0]);
+        int previous = 0;
+        int current = 1;
+        int next = previous + current;
+        int limit = Integer.parseInt(args[0]);
+
+        System.out.println("--FIBONNACI CHALLENGE--");
+        do {
+            System.out.println(previous);
+            previous = current;
+            current = next;
+            next = previous + current;
+        } while (current < limit);
+        System.out.println("--FINISHED--");
+
     }
 }
