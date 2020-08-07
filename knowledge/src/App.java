46765391e7ff2f32ddb073678d7ad60971cdc9e7
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-07 10:06:46 -0300

    Treinando singleton

--- knowledge/src/App.java
+++ knowledge/src/App.java
@@ -1,5 +1,10 @@
+import utils.Door;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Door.getInstance().getStatus();
+        Door.getInstance().toClose();
+        Door.getInstance().getStatus();
+        Door.getInstance().toOpen();
+        Door.getInstance().getStatus();
     }
 }
