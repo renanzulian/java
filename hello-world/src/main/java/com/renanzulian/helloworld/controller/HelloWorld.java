7975c280c81681bf6ae6e1daa09bda3d5be1541b
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-09 14:29:05 -0300

    Criando projeto maven e gerando um Jar

--- hello-world/src/main/java/com/renanzulian/helloworld/controller/HelloWorld.java
+++ hello-world/src/main/java/com/renanzulian/helloworld/controller/HelloWorld.java
@@ -2,7 +2,6 @@ package com.renanzulian.helloworld.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
