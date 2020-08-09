1bf18de2987b3f13b355434cc86c025370eab720
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-09 15:30:30 -0300

    Lendo um arquivo

---
+++ quake-parser-challenge/src/main/java/com/renanzulian/quakeparser/App.java
@@ -0,0 +1,24 @@
+package com.renanzulian.quakeparser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws FileNotFoundException {
+        File source = new File("/home/renanferreira/Desktop/java/quake-parser-challenge/games.log");
+        Scanner sc = new Scanner(source);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());             
+        }
+        
+        sc.close();
+        
+    }
+}
