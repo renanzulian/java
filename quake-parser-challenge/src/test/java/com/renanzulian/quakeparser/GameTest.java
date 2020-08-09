313860fbde89d51ec252117d65714efe904722d8
Renan Zulian <renanzulian@gmail.com>
2020-08-09 17:01:08 -0300

    Criando instancia de um jogo

--- quake-parser-challenge/src/test/java/com/renanzulian/quakeparser/GameTest.java
+++ quake-parser-challenge/src/test/java/com/renanzulian/quakeparser/GameTest.java
@@ -1,5 +1,21 @@
 package com.renanzulian.quakeparser;
 
+import static org.junit.Assert.assertTrue;
+
+import com.github.javafaker.Faker;
+
+import org.junit.Test;
+
 public class GameTest {
-    
+    Faker faker = new Faker();
+
+    private Game gameFactory() {
+        return new Game();
+    }
+
+    @Test
+    public void shouldInstanceADefaultGame() {
+        Game game = this.gameFactory();
+        assertTrue(game instanceof Game);
+    }
 }
