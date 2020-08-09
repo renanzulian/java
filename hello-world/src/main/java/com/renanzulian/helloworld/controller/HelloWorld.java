cf88a7d279754d4fa8ece29ffdf12b68a2ff8f79
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-09 13:33:08 -0300

    Criando um controller

---
+++ hello-world/src/main/java/com/renanzulian/helloworld/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.renanzulian.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloWorld {
+    
+    @GetMapping(value="/")
+    public String getAll() {
+        return "HelloWorld Spring";
+    }
+    
+}
