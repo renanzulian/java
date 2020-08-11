e21eab3f77db641622e981c99c2e7e5a77fcecb9
Renan Pedro Zulian Ferreira <renan.ferreira@enext.com.br>
2020-08-11 09:38:12 -0300

    Criando um usuário no banco de dados

--- connection-factory/src/App.java
+++ connection-factory/src/App.java
@@ -1,9 +1,23 @@
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Connection conn = new ConnectionFactory().getConnection();
         System.out.println("Database connected!");
+        String query = "INSERT INTO contatos"+
+        " (nome, email, endereco, dataNascimento)"+
+        " values(?, ?, ?, ?)";
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, "Renan");
+        stmt.setString(2, "renanzulian@gmail.com");
+        stmt.setString(3, "rua jose dos santos 19");
+        stmt.setDate(4, new Date(Calendar.getInstance().getTimeInMillis()));
+        stmt.execute();
+        stmt.close();
+        System.out.println("Contato has been saved!");
         conn.close();
     }
 }
