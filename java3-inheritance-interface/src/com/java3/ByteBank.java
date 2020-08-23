ffec3ee6a48091b81ce9c79dd24332eb34dd3dd6
Renan Zulian <renanzulian@gmail.com>
2020-08-23 15:27:42 -0300

    Inheritance on ByteBank

---
+++ java3-inheritance-interface/src/com/java3/ByteBank.java
@@ -0,0 +1,10 @@
+package com.java3;
+
+public class ByteBank {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("Renan");
+        employee.setSalary(1250.00);
+        System.out.println(employee.getBonus());
+    }
+}
---
+++ java3-inheritance-interface/src/com/java3/Employee.java
@@ -0,0 +1,36 @@
+package com.java3;
+
+public class Employee {
+
+    private String name;
+    private String cpf;
+    private double salary;
+
+    public double getBonus() {
+        return this.salary * 0.1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
